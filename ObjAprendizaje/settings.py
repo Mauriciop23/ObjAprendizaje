b806347acccab0a5d5256695509c9b1e585caa9e
Mauriciop23 <18121522@morelia.tecnm.mx>
2022-06-18 19:36:42 -0500

    Start App

--- ObjAprendizaje/settings.py
+++ ObjAprendizaje/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'ObjAprendizaje.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'obj_db',
+        'USER': 'postgres',
+        'PASSWORD': 'daniela',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
