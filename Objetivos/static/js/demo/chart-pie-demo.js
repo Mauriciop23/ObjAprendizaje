c9c6c5ddbc638dfeac2324b0f109f2bd1eb54def
Mauriciop23 <18121522@morelia.tecnm.mx>
2022-06-23 22:12:53 -0500

    Index3

--- Objetivos/static/js/demo/chart-pie-demo.js
+++ Objetivos/static/js/demo/chart-pie-demo.js
@@ -7,10 +7,10 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Buena", "Mala", "Neutral"],
     datasets: [{
       data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
+      backgroundColor: ['#00002D', '#1CD794', '#78182F'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
     }],
