a968d75cd942261ab9cc0dca236c8d34b1e33fc0
Mauriciop23 <18121522@morelia.tecnm.mx>
2022-07-13 21:15:06 -0500

    Avance3

--- ObjAprendizaje/urls.py
+++ ObjAprendizaje/urls.py
@@ -23,5 +23,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Objetivos.views.home, name="home"),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'), 
-    path('registro/', Objetivos.views.registro, name="registro")
+    path('registro/', Objetivos.views.registro, name="registro"),
+    path('dashboard/', Objetivos.views.dashboardGeneral, name="dashboardGeneral"),
+    path('dashboard/mi_contenido', Objetivos.views.dashboardContenido, name="dashboardContenido"),
+    path('objeto/', Objetivos.views.objeto, name="objeto"),
 ]
--- Objetivos/views.py
+++ Objetivos/views.py
@@ -5,3 +5,12 @@ def home(request):
 
 def registro(request):
     return render(request, 'register.html')
+
+def dashboardGeneral(request):
+    return render(request, 'dashboard_general.html')
+
+def dashboardContenido(request):
+    return render(request, 'dashboard_contenido.html')
+
+def objeto(request):
+    return render(request, 'objeto.html')
