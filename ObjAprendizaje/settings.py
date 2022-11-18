93187fb991fd4c700bcb258c6669c7c5d08f3dd7
Mauriciop23 <18121522@morelia.tecnm.mx>
2022-11-17 21:14:17 -0600

    aws

--- ObjAprendizaje/settings.py
+++ ObjAprendizaje/settings.py
@@ -76,16 +76,29 @@ WSGI_APPLICATION = 'ObjAprendizaje.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'obj_db',
-        'USER': 'postgres',
-        'PASSWORD': 'daniela',
-        'HOST': 'localhost',
-        'PORT': '5432',
+if not DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'obj_db',
+            'USER': 'postgres',
+            'PASSWORD': 'daniela',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'postgres',
+            'USER': 'postgres',
+            'PASSWORD': 'ObjetosMaster2022$',
+            'HOST': 'objdb.c9uqpg886ztz.us-east-1.rds.amazonaws.com',
+            'PORT': '5432',
+        }
+    }
+
 
 
 # Password validation
